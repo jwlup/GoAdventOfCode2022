5c4c2b0ae4be9dd0fcbdf672513414cda7e47b91
jwlup <jwolf_mail@gmx.de>
2023-08-02 23:00:09 +0200

    ADD basic verbose solution

---
+++ 2/day2.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"os"
+	"strings"
+)
+
+func score_round(round string) (int, error) {
+	actions := strings.Split(round, " ")
+	if len(actions) != 2 {
+		return -1, errors.New("Not a valid round - expect exactly 2 actions")
+	}
+	shape_score := map[string]int{
+		"X": 1,
+		"Y": 2,
+		"Z": 3,
+	}
+	score, err := outcome_score(actions)
+	if err != nil {
+		return -1, err
+	}
+	score += shape_score[actions[1]]
+	return score, nil
+}
+
+func outcome_score(actions []string) (int, error) {
+	results := 0
+	your_shape := actions[1]
+	switch actions[0] {
+	case "A":
+		if your_shape == "Y" {
+			results = 6
+		} else if your_shape == "X" {
+			results = 3
+		}
+	case "B":
+		if your_shape == "Z" {
+			results = 6
+		} else if your_shape == "Y" {
+			results = 3
+		}
+	case "C":
+		if your_shape == "X" {
+			results = 6
+		} else if your_shape == "Z" {
+			results = 3
+		}
+	default:
+		return -1, errors.New("Invalid action by opponent")
+	}
+	return results, nil
+}
+
+func main() {
+	content, err := os.ReadFile("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	per_round := strings.Split(string(content), "\n")
+
+	total_score := 0
+	for _, round := range per_round {
+		score, err := score_round(round)
+		if err != nil {
+			log.Fatal(err)
+		}
+		total_score += score
+	}
+	println(total_score)
+}
