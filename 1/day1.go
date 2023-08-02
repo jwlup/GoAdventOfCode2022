083f725b52ca4894900dd36007d921ac43064bbd
jwlup <jwolf_mail@gmx.de>
2023-08-02 21:43:42 +0200

    ADD full functionality to helper function and main solution

--- 1/day1.go
+++ 1/day1.go
@@ -3,11 +3,21 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func get_elf_total(per_elf string) int {
+func get_elf_total(per_elf string) (int, error) {
 	per_snack := strings.Split(per_elf, "\n")
+	total := 0
+	for _, snack := range per_snack {
+		calories, err := strconv.Atoi(snack)
+		if err != nil {
+			return -1, err
+		}
+		total += calories
+	}
+	return total, nil
 }
 
 func main() {
@@ -18,4 +28,18 @@ func main() {
 
 	per_elf := strings.Split(string(content), "\n\n")
 
+	max_per_elf := 0
+	max_elf := 0
+
+	for elf_index, elf := range per_elf {
+		per_elf, err := get_elf_total(elf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if per_elf > max_per_elf {
+			max_elf = elf_index
+			max_per_elf = per_elf
+		}
+	}
+	println("%v,%v", max_per_elf, max_elf+1)
 }
